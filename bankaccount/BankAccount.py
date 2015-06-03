92dba9535e7cdcda5ffde462884c297e72832ddd
IvoT <ivo.t@mail.bg>
2015-06-03 18:40:32 +0300

    solutions

---
+++ bankaccount/BankAccount.py
@@ -0,0 +1,52 @@
+class BankAccount():
+    def __init__(self, name, balance, currency):
+        self.name = str(name)
+        self.balance = balance
+        self.currency = str(currency)
+        self.history = []
+
+        self.make_history("Account was created")
+
+    def make_history(self, event):
+        self.history.append(event)
+
+    def __str__(self):
+        return "Bank account for {} with balance of {} {}" .format(self.name, self.balance, self.currency)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def currency(self):
+        return self.currency
+
+    def holder(self):
+        return self.name
+
+    def balance(self):
+        self.make_history("balance check -> {}{}" .format(self.balance, self.currency))
+
+        return self.balance
+
+    def withdraw(self, amount):
+        if self.balance - amount < 0:
+            return False
+        else:
+            self.balance - amount >= 0
+            self.balance -= amount
+            return True
+
+
+    def transferTo(self, account, amount):
+        if (self.balance >= amount):
+            self.balance -= amount
+            account.balance += amount
+            return True
+        else:
+            return False
+
+    def history(self):
+        return self.history
